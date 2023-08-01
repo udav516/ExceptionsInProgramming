9e62b85f9d0f0224f181e9d732b13b4222307b15
udav516 <eldar.udav516@yandex.ru>
2023-08-01 22:58:40 +0300

    add task 2 to homework2

--- src/main/java/org/example/Homework2.java
+++ src/main/java/org/example/Homework2.java
@@ -6,6 +6,27 @@ public class Homework2 {
     public static void main(String[] args) {
         float input = getInputFloat();
         System.out.println("Введенное число: " + input);
+
+        /**
+         * Задание 2
+         * try {
+         *    int d = 0;
+         *    double catchedRes1 = intArray[8] / d;
+         *    System.out.println("catchedRes1 = " + catchedRes1);
+         * } catch (ArithmeticException e) {
+         *    System.out.println("Catching exception: " + e);
+         * }
+         * Исправить код
+         */
+        try {
+//            int d = 0;
+            int d = 3;
+            int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            double catchRes1 = (double) intArray[8] / d;
+            System.out.println("catchRes1 = " + catchRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
     }
 
     /**
