4f45c02ab0f42eb7620f973404a9db4208d0e34b
udav516 <eldar.udav516@yandex.ru>
2023-08-01 22:13:46 +0300

    add task 1 to homework2

---
+++ src/main/java/org/example/Homework2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.*;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        float input = getInputFloat();
+        System.out.println("Введенное число: " + input);
+    }
+
+    /**
+     * Проверка на ввод дробного числа(типа float)
+     * @return дробное число
+     */
+    public static float getInputFloat() {
+        Scanner scanner = new Scanner(System.in);
+        float input = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                System.out.print("Введите дробное число: ");
+                input = Float.parseFloat(scanner.nextLine());
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка ввода. Повторите попытку.");
+            }
+        }
+
+        return input;
+    }
+}
