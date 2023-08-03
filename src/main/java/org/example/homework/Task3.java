c8850ca7cc89110bf4c737831f7ebdd8e8fe428f
udav516 <eldar.udav516@yandex.ru>
2023-08-03 13:02:07 +0300

    fix Task3

--- src/main/java/org/example/homework/Task3.java
+++ src/main/java/org/example/homework/Task3.java
@@ -30,7 +30,7 @@ public class Task3 {
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
-            int[] abc = {1, 2, 3, 4};
+            int[] abc = {1, 2};
             abc[3] = 9;
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
