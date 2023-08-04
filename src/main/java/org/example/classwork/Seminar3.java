7c8947ea597daf30ca62e4b3a83606909ad014e8
udav516 <eldar.udav516@yandex.ru>
2023-08-04 19:14:18 +0300

    add Task1

--- src/main/java/org/example/classwork/Seminar3.java
+++ src/main/java/org/example/classwork/Seminar3.java
@@ -4,11 +4,23 @@ import java.io.*;
 import java.nio.file.*;
 
 public class Seminar3 {
+    public static void main(String[] args) {
+        try {
+            doSomething();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void doSomething() throws Exception {
+
+    }
+
     /**
      * Использование блока try-with-resources
-     * @param pathRead считывающий файл
+     * @param pathRead  считывающий файл
      * @param pathWrite записываемый файл
-     * @throws IOException
+     * @throws IOException ошибка при выводе/вводе данных
      */
     public void rwLine(Path pathRead, Path pathWrite) throws IOException {
         try (BufferedReader in = Files.newBufferedReader(pathRead);
