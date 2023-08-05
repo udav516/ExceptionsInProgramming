e31d07ffd428839244a91dbf784dc99aaa35efcb
udav516 <eldar.udav516@yandex.ru>
2023-08-05 15:40:48 +0300

    add validateData, try-catch and data quantity condition

--- src/main/java/org/example/homework/UserDataApp.java
+++ src/main/java/org/example/homework/UserDataApp.java
@@ -11,7 +11,12 @@ public class UserDataApp {
         System.out.println("Иванов Иван Иванович 12.12.2012 79857932846 f(женский)/m(мужской)");
         String userData = scanner.nextLine();
         String[] userDataArray = userData.split(" ");
-        if (userDataArray.length != 6) {
+        if (userDataArray.length < 6) {
+            System.out.println("Недостаточно данных");
+            return;
+        } else if (userDataArray.length > 6) {
+            System.out.println("Избыток данных");
+            return;
         }
         String lastName = userDataArray[0];
         String firstName = userDataArray[1];
@@ -21,10 +26,15 @@ public class UserDataApp {
         String gender = userDataArray[5];
 
         try {
-
-        } catch () {
-
+            validateData(lastName, firstName, middleName, dateOfBirth, phoneNumber, gender);
+            saveUserDataToFile(lastName, firstName, middleName, dateOfBirth, phoneNumber, gender);
+            System.out.println("Данные успешно сохранены в файл");
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            e.printStackTrace();
         }
+
+        scanner.close();
     }
 
     /**
@@ -37,7 +47,28 @@ public class UserDataApp {
      * @param gender      пол
      */
     private static void validateData(String lastName, String firstName, String middleName, String dateOfBirth, String phoneNumber, String gender) {
+        if (!lastName.matches("[A-Za-z]+") && !lastName.matches("[А-Яа-я]+") ) {
+            throw new IllegalArgumentException("Неверный формат фамилии, введите буквами.");
+        }
+        if (!firstName.matches("[A-Za-z]+") && !firstName.matches("[А-Яа-я]+")) {
+            throw new IllegalArgumentException("Неверный формат имени, введите буквами.");
+        }
+        if (!middleName.matches("[A-Za-z]+") && !middleName.matches("[А-Яа-я]+")) {
+            throw new IllegalArgumentException("Неверный формат отчества, введите буквами.");
+        }
+        if (!dateOfBirth.matches("(((0[1-9]|[12][0-9]|3[01]).((0[13578]|1[02]))|((0[1-9]|[12][0-9]|30).(0[469]|11))" +
+                "|(0[1-9]|[1][0-9]|2[0-8]).(02))" +
+                ".([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3}))" +
+                "|(29.02.(([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00)))")) {
+            throw new IllegalArgumentException("Неверный формат даты рождения, введите верно dd.mm.yyyy");
+        }
 
+        if (!phoneNumber.matches("\\d+")) {
+            throw new IllegalArgumentException("Неверный формат номера телефона, введите верно 9857932846");
+        }
+        if (!gender.equals("f") && !gender.equals("m")) {
+            throw new IllegalArgumentException("Неверный формат пола, введите верно f(женский) или m(мужской)");
+        }
     }
 
     /**
@@ -50,8 +81,8 @@ public class UserDataApp {
      * @param gender      пол
      */
     private static void saveUserDataToFile(String lastName, String firstName, String middleName, String dateOfBirth, String phoneNumber, String gender) throws IOException {
-        String fileName = "dataUser";
-        try (FileWriter fileWriter = new FileWriter(fileName, true)){
+        String fileName = "C:\\Users\\Eldar\\IdeaProjects\\ExceptionsInProgramming\\src\\main\\java\\org\\example\\homework\\dataUser";
+        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
             fileWriter.write(lastName + " " + firstName + " " + middleName + " " + dateOfBirth + " " + phoneNumber + " " + gender + "\n");
         }
     }
